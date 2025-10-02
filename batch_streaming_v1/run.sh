26620089353cf7396a81ff608ef821be803346ab
Praful Rana <prafulrana@gmail.com>
2025-10-03 08:49:21 +1000

    run.sh: default X264_THREADS=2 to reduce x264 contention by default

--- batch_streaming_v1/run.sh
+++ batch_streaming_v1/run.sh
@@ -13,6 +13,7 @@ cmd=(docker run --rm \
   -e RTSP_PORT="$RTSP_PORT" \
   -e BASE_UDP_PORT="${BASE_UDP_PORT:-5000}" \
   -e PUBLIC_HOST="${PUBLIC_HOST:-10.243.249.215}" \
+  -e X264_THREADS="${X264_THREADS:-2}" \
   -v "$ENGINE_DIR":/models \
   batch_streaming:latest)
 
